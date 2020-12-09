62eb5758ce75a38c04d3f3a620c4d39b6d529bef
Ashutosh Singh <Ashutosh Singh@LAPTOP-E2IEQ3CH>
2020-12-09 20:55:55 +0530

    added @Test annotation

--- src/main/java/TestClass/Test2.java
+++ src/main/java/TestClass/Test2.java
@@ -1,14 +1,16 @@
 package TestClass;
 
+import org.testng.annotations.Test;
+
 public class Test2 {
 
-	
+	@Test
 	public void test2() {
 		
 		
 		System.out.println("TEST-2 Executed Successfully");
 	}
-	
+	@Test
 	public void test3() {
 		
 		
